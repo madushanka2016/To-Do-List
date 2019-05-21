51da225bec99285357c324ef1bc9375ae25c1184
madushanka2016 <madushankakahawa@gmail.com>
2019-05-22 01:01:12 +0530

    add fire base to enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpAjcpmOrYvz2gwsjLYKXxxb_XLq5Xncw",
+    authDomain: "to-do-list-c044b.firebaseapp.com",
+    databaseURL: "https://to-do-list-c044b.firebaseio.com",
+    projectId: "to-do-list-c044b",
+    storageBucket: "to-do-list-c044b.appspot.com",
+    messagingSenderId: "43993650083",
+    appId: "1:43993650083:web:dc876a1de4aa0ebb"
+  }
 };
